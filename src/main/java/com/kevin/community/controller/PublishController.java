732d1cd9aef40bc541f2dda5e77cef00ceace14e
dk976 <1334483896@qq.com>
2019-11-23 17:46:08 +0800

    question detail

--- src/main/java/com/kevin/community/controller/PublishController.java
+++ src/main/java/com/kevin/community/controller/PublishController.java
@@ -1,7 +1,6 @@
 package com.kevin.community.controller;
 
 import com.kevin.community.mapper.QuestionMapper;
-import com.kevin.community.mapper.UserMapper;
 import com.kevin.community.model.Question;
 import com.kevin.community.model.User;
 import org.jetbrains.annotations.NotNull;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
@@ -20,8 +18,6 @@ public class PublishController {
 
     @Autowired
     private QuestionMapper questionMapper;
-    @Autowired
-    private UserMapper userMapper;
 
     @GetMapping("/publish")
     public String publish(){
@@ -50,20 +46,7 @@ public class PublishController {
             model.addAttribute("error","标签不能为空");
             return "publish";
         }
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies !=null && cookies.length != 0)
-        for (Cookie cookie : cookies){
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                user = userMapper.findByToken(token);
-                if (user != null){
-                    System.out.println("------------发布成功---------------");
-                    request.getSession().setAttribute("user",user);
-                }
-                break;
-            }
-        }
+        User user = (User)request.getSession().getAttribute("user");
         if (user == null){
             model.addAttribute("error","用户未登录");
             System.out.println("**********用户未登录***********");
